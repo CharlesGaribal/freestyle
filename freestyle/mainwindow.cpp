03a760a200d3100209205024d3dac83af2107510
Exoria <matt.dachy@gmail.com>
2015-02-25 12:51:51 +0100

    TopologicalHandler Debug

--- freestyle/mainwindow.cpp
+++ freestyle/mainwindow.cpp
@@ -83,7 +83,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent),
     addAction(ui->actionAbout);
 
     //loadFile("../data/bimba.off");
-    loadFile("../data/1sphere.dae");
+    loadFile("../data/sphere_bis.obj");
 }
 
 MainWindow::~MainWindow() {
